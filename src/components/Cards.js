33d87a31e2668c9822efaf1e666d6e0e1d3f878c
Terrell Cooper <TerrellCoop@gmail.com>
2021-03-01 11:42:17 -0700

    added pictures from projects

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='./images/img-9.jpg'
+              src="images/Lets_chat_pic"
               text='Lets Chat - An app that creates a global chatroom'
               label='Project'
               path='/projects'
             />
             <CardItem
-              // src='images/img-2.jpg'
+              src='images/img-2.jpg'
               text='ReClip - This app is Snapchat clone with a little twist'
               label='Project'
               path='/projects'
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              // src='images/img-3.jpg'
+              src='images/img-3.jpg'
               text='Weather App - Pull the weather from any with simple search'
               label='Project'
               path='/project'
             />
             <CardItem
-              // src='images/img-4.jpg'
+              src='images/img-4.jpg'
               text='A show case of my experince and skill'
               label='Resume'
               path='/resume'
             />
             <CardItem
-              // src='images/img-8.jpg'
+              src='images/img-8.jpg'
               text='Brick Breaker - Ready to play classic game of Brick Breaker'
               label='Project'
               path='/project'
