618a72762225c44345c35d81f186224df4abd2a8
Terrell Cooper <TerrellCoop@gmail.com>
2021-03-22 12:30:13 -0600

    changed some styling for resume and contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import '../../App.css';
+import CardItem from '../CardItem';
+import Footer from '../Footer';
 
 export default function Contact() {
-  return <h1 className='contact'>Contact</h1>;
+  return (
+    <>
+      <h1 className='contact'>Contact</h1>
+      <CardItem>
+        
+      </CardItem>
+      <Footer/>
+    </>
+  )
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import '../../App.css';
+import CardItem from '../CardItem';
+import Footer from '../Footer';
 
 export default function Resume() {
-  return <h1 className='resume'>Resume</h1>;
+  return (
+    <>
+    <h1 className='resume'>Resume</h1>
+    <CardItem></CardItem>
+    <Footer/>
+    </>
+  )
+
+
 }
